33c3d343ea50c17e3e8f2b8bcf8d1ed77544956c
Guyl <guylorinczi1@gmail.com>
2024-06-04 20:46:20 +0300

    fix .sh file

--- RunningScripts/clone_and_init_Guy-lor.sh
+++ RunningScripts/clone_and_init_Guy-lor.sh
@@ -1,12 +1,14 @@
 # cloning your code from github:
 git clone https://github.com/Guy-Lor/HumanChoicePrediction.git
 cd HumanChoicePrediction
-conda env create -f final_project_env.yml
-conda activate final_project_env
+
+conda env create -f final_project_env_Guy-lor.yml
+conda activate final_project_env_Guy-lor
+wandb login
 # Your main sweep:
 python final_sweep_Guy-lor.py
 
 # More runs appear in your report:
-# python sweep_1.py
-# python sweep_2.py
+python sweep_1_HPT_ON_Guy-lor.py
+python sweep_2_HPT_ON_Guy-lor.py
 # python sweep_3.py
